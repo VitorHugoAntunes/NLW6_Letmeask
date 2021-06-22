6c9a3e61c5a0dfe2677ae42563fd12dfeb40ba8f
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-06-22 19:43:47 -0300

    Authentication Context with Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,20 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+
 import '../styles/auth.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 export function Home(){
     const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-    function navigateToNewRoom(){
+    async function handleCreateRoom(){
+        if(!user) {
+            await signInWithGoogle();
+        }
+
         history.push('/rooms/new')
     }
     return(
@@ -24,7 +32,7 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo da Google" />
                         Crie uma sala com o Google
                     </button>
