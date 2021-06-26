e9d15cdf6d418e72c48850e730f0d3387a99dfcf
VitorHugoAntunes <vitor.passos3@etec.sp.gov.br>
2021-06-25 23:25:11 -0300

    Copied Code Confirmation Toast

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,4 +1,5 @@
 import copyImg from '../../assets/images/copy.svg';
+import toast, { Toaster } from 'react-hot-toast';
 
 import './styles.scss';
 
@@ -9,13 +10,17 @@ type RoomCodeProps = {
 export function RoomCode(props: RoomCodeProps){
     function copyRoomCodeToClipboard(){
         navigator.clipboard.writeText(props.code)
+        toast.success('Código copiado.')
     }
     return(
-        <button className="room-code" onClick={copyRoomCodeToClipboard}>
-            <div>
-                <img src={copyImg} alt="Copy room code" />
-            </div>
-            <span>Sala #{props.code}</span>
-        </button>
+        <>
+            <button className="room-code" onClick={copyRoomCodeToClipboard}>
+                <div>
+                    <img src={copyImg} alt="Copy room code" />
+                </div>
+                <span>Sala #{props.code}</span>
+            </button>
+            <Toaster />
+        </>
     )
 }
